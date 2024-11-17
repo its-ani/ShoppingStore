962f906761b95fdf279f772a14cc7f50fa5614fa
its-ani <guptaanirudh712@gmail.com>
2024-11-17 17:40:14 +0530

    Return status (Response Entity)

--- src/main/java/dev/anirudh/productservicenov24/controllers/ProductController.java
+++ src/main/java/dev/anirudh/productservicenov24/controllers/ProductController.java
@@ -3,6 +3,9 @@ package dev.anirudh.productservicenov24.controllers;
 import dev.anirudh.productservicenov24.dtos.CreateProductRequestDto;
 import dev.anirudh.productservicenov24.models.Product;
 import dev.anirudh.productservicenov24.services.ProductService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
@@ -22,8 +25,17 @@ public class ProductController {
     }
 
     @GetMapping("/products/{id}")
-    public Product getSingleProduct(@PathVariable("id") long id){
-        return productService.getSingleProduct(id);
+    public ResponseEntity<Product> getSingleProduct(@PathVariable("id") long id){
+        Product p = productService.getSingleProduct(id);
+        ResponseEntity<Product> responseEntity;
+        if(p != null){
+            responseEntity = new ResponseEntity<>(p, HttpStatus.OK);
+        }
+        else{
+            responseEntity = new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+
+        return responseEntity;
     }
 
     @PostMapping("/products")
