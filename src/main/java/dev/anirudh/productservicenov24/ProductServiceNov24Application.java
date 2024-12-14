b86b059715a0bf9b51af977f2ce086eed9718f3f
its-ani <guptaanirudh712@gmail.com>
2024-12-14 17:21:57 +0530

    Exception Handling

--- src/main/java/dev/anirudh/productservicenov24/ProductServiceNov24Application.java
+++ src/main/java/dev/anirudh/productservicenov24/ProductServiceNov24Application.java
@@ -6,10 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ProductServiceNov24Application {
-
     public static void main(String[] args) {
-
         SpringApplication.run(ProductServiceNov24Application.class, args);
     }
-
 }
